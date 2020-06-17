d981e1635cde938579c779022684c3d5bec014b6
Gage Thornberry <gagedoug13@gmail.com>
2020-06-17 04:44:50 -0600

    login working with code in address bar

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import React from 'react';
 import './App.css';
 import Main from './components/Main'
 
+
 function App() {
 
   let code = window.location.search.split('&')[0].split('=')[1]
 
 
-  if (code) {
-    fetch(`http://192.168.1.117:3001/useroptions?code=${code}`, {
+  // if (code) {
+  //   fetch(`http://192.168.1.117:3001/useroptions?code=${code}`, {
+  //     // mode: 'no-cors',
+  //     credentials: 'include'
+  //   })
+  //   .then(response => response.json())
+  //   .then(data => console.log(data))   
+  // } else {
+  //   console.log('no code was present in the window')
+  // }
+
+  fetch(`http://192.168.1.117:3001/useroptions?${code ? 'code=' + code : ''}`, {
       // mode: 'no-cors',
       credentials: 'include'
     })
     .then(response => response.json())
     .then(data => console.log(data))
-    .then(console.log(document.cookie, 'cookies'))
-  } else {
-    console.log('no code was present in the window')
-  }
 
 
 
