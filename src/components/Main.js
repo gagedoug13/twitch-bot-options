9b684b9e83d651e0a75b213b83de84df8e410604
Gage Thornberry <gagedoug13@gmail.com>
2020-06-18 00:34:07 -0600

    styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,7 @@ export default function Main() {
     return (
         <div className='mainDiv'>
             <div className='robotIntroduction'>
-                <h1>Robot Ape is a Twitch bot like no other. Customize your stream with trivia commands, history, secret word, and much more.</h1>
+                <h1 className='font1'>Robot Ape is a Twitch bot like no other. Customize your stream with trivia commands, history, secret word, and much more.</h1>
             </div>
         </div>
     )
