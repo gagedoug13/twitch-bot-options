0163f1f17cc8ecc5e250b195c752ce35b5149034
Gage Thornberry <gagedoug13@gmail.com>
2020-06-23 17:20:28 -0600

    Made the app component stateful and is updating state with the initial fetch successfully.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 import Main from './components/Main'
 
 export default class App extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      options: null
+    };
+  }
+  
   
   componentDidMount() {
     let code = window.location.search.split('&')[0].split('=')[1]
@@ -16,13 +22,18 @@ export default class App extends Component {
       credentials: 'include'
     })
       .then(response => response.json())
-      .then(data => options = data.options)
+      .then(data => {
+        options = data.options
+        this.setState({
+          options: data.options
+        })
+      })
   
-  }
   
-  updateOptions = async () => {
+  
+ this.updateOptions = async () => {
     options.history = true
-    console.log(options)
+    console.log(options, "options line 28")
     fetch('http://localhost:3001/update', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -31,11 +42,14 @@ export default class App extends Component {
     })
   }
 
+}
+
   render() {
+    console.log(this.state.options)
     return (
       <div>
         <h1>welcome</h1>
-      <button onClick={updateOptions}>update</button>
+      <button onClick={this.updateOptions}>update</button>
       <Main />
       </div>
     )
