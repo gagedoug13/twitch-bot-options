3b7c6bb298ec3eb223d708f7dc9173558356b504
Gage Thornberry <gagedoug13@gmail.com>
2020-06-25 16:27:42 -0600

    options page is ready to update state

---
+++ src/components/BotButtonsContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function BotButtonsContainer() {
+    return (
+        <div>
+            <button className='signOutButton'>Sign Out</button>
+
+            <button className='disconnectButton'>Disconnect From Bot</button>
+        </div>
+    )
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,15 @@
 import React from 'react'
 // import Nav from './Nav'
 import OptionsContainer from './OptionsContainer'
+import BotButtonsContainer from './BotButtonsContainer'
 
 export default function Main() {
     return (
         <div className='mainDiv'>
             {/* <Nav /> */}
             <OptionsContainer />
+
+            <BotButtonsContainer />
 
             {/* <div className='robotIntroduction'> */}
                 {/* <h1 className='font1'>Robot Ape is a Twitch bot like no other. Customize your stream with trivia commands, history, secret word, and much more.</h1> */}
