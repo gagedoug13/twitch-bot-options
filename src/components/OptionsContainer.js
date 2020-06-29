3082c062c1ce8dfcaf6c0ed1cc71807d8ffbb1e9
Gage Thornberry <gagedoug13@gmail.com>
2020-06-29 14:24:10 -0600

    took out question mark

--- src/components/OptionsContainer.js
+++ src/components/OptionsContainer.js
@@ -15,7 +15,7 @@ export default function OptionsContainer(props) {
             <OtherContainer currentOptions={props.currentOptions} switchHandler={props.switchHandler}/>
             
             {props.areIdenticalObjects() && <SaveButton saveChanges={props.saveChanges} />}
-            <img className='questionMark' alt='couldnt get question mark' src='https://lh3.googleusercontent.com/proxy/1iZniiTYRe0ikI1NjpyMMBichcnl7kWJQi8ZB26IP7Fp_ShN-mJraal0CojyYOrNK_FUXkRmX7DKwrUqn4qUWxhD5yWBVVV4gOgu_4iJinLcqTuAbY15IeBMR5DeWiaV_GMpIRNZajnEvHVsJqqknWrwhnUT49ppBTyaTqz4S_eSeuJ0IZxyQJdl-tfz0g'></img>
+           
             
         </div>
     )
