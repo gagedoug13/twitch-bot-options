6a4dd6e28b2877d8d64da45f1d3cc3bb27b3b00f
Gage Thornberry <gagedoug13@gmail.com>
2020-06-30 14:39:36 -0600

    Styled save button. Feature is fully complete.

--- src/components/SaveButton.js
+++ src/components/SaveButton.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SaveOptionsButton(props) {
     return (
         <div>
-            <button onClick={props.saveChanges}>Save Options</button>
+            <button className='saveButton' onClick={props.saveChanges}>Save Options</button>
         </div>
     )
 }
