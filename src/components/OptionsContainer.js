47aed54f453e47261468f2f70e1d1a602faeb466
Gage Thornberry <gagedoug13@gmail.com>
2020-06-30 14:27:33 -0600

    fixed the sizing issue

--- src/components/OptionsContainer.js
+++ src/components/OptionsContainer.js
@@ -7,11 +7,11 @@ import SaveButton from './SaveButton'
 export default function OptionsContainer(props) {
     return (
         <div className='optionsContainer'>
-            <h1>Commands</h1>
+            <h1 className='optionsTitle'>Commands</h1>
             <CommandsContainer currentOptions={props.currentOptions} switchHandler={props.switchHandler} />
-            <h1>Channel Points</h1>
+            <h1 className='optionsTitle'>Channel Points</h1>
             <ChannelPointsContainer currentOptions={props.currentOptions} switchHandler={props.switchHandler}/>
-            <h1>Other Options</h1>
+            <h1 className='optionsTitle'>Other Options</h1>
             <OtherContainer currentOptions={props.currentOptions} switchHandler={props.switchHandler}/>
             
             {props.areIdenticalObjects() && <SaveButton saveChanges={props.saveChanges} />}
