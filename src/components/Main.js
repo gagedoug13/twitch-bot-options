fd84cbd0ac2188e509670a100a050c943a454e16
Gage Thornberry <gagedoug13@gmail.com>
2020-07-31 12:40:25 -0600

    cleaning up code

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,6 @@ import BotButtonsContainer from './BotButtonsContainer'
 export default function Main(props) {
     return (
         <div className='mainDiv'>
-            
             <OptionsContainer data={props.data} 
                               switchHandler={props.switchHandler}
                               currentOptions={props.currentOptions}
@@ -14,10 +13,6 @@ export default function Main(props) {
                               saveChanges={props.saveChanges} />
 
             <BotButtonsContainer />
-
-            {/* <div className='robotIntroduction'> */}
-                {/* <h1 className='font1'>Robot Ape is a Twitch bot like no other. Customize your stream with trivia commands, history, secret word, and much more.</h1> */}
-            {/* </div> */}
         </div>
     )
 }
